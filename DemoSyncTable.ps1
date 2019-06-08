5c42a46566813f63274047ade6bad2405a5ba405
Adam <adambacon1@hotmail.com>
2019-06-08 12:34:25 +0100

    Add files via upload
    changed to 1 second refresh leads to smoother loading of the table

--- DemoSyncTable.ps1
+++ DemoSyncTable.ps1
@@ -3,12 +3,12 @@ Import-Module -Name UniversalDashboard
 ###Stop any running dashboards so no conflict with port numbers
 Get-UDDashboard | Stop-UDDashboard
 ######## Set a schedule for the endpoint to run every xxx
-$Schedule = New-UDEndpointSchedule -Every 5 -Second
+$Schedule = New-UDEndpointSchedule -Every 1 -Second
 ####initialise the endpoint variable to load when the dashboard loads...you can include functions, and use the cache variable very important
 $Endpoint = New-UDEndpoint -Schedule $Schedule -Endpoint {
 
-    $Cache:process = Get-Process | select -first 6 | Out-UDTableData -Property @("name", "id")
-    $Cache:service = Get-Service | select -first 6 | Out-UDTableData -Property @("DisplayName", "status")
+    $Cache:process = Get-Process | select -first 5 | Out-UDTableData -Property @("name", "id")
+    $Cache:service = Get-Service | select -first 5 | Out-UDTableData -Property @("DisplayName", "status")
 }
 $test = New-UDPage -Name 'Test' -Content {
     $Layout = '{"lg":[{"w":4,"h":10,"x":0,"y":0,"i":"grid-element-leftcard","moved":false,"static":false},{"w":7,"h":10,"x":4,"y":0,"i":"grid-element-table","moved":false,"static":false},{"w":1,"h":2,"x":11,"y":0,"i":"grid-element-icon1","moved":false,"static":false}]}'
@@ -28,7 +28,7 @@ $test = New-UDPage -Name 'Test' -Content {
                 elseif ($Session:Ticker -match 'service') { $Cache:service }
                 elseif ($Session:Ticker -match 'process') { $Cache:process }
                 else { Show-UDToast -Message "You can only type process or service try again" -Duration 30000 }
-            } -RefreshInterval 5
+            } -RefreshInterval 1
             New-UDIcon -Color "#7ed321" -Icon "question" -Id "icon1" -Size "5x"
         }
     }
